f1b9f2085ba9447b864b2beb52eb3c8950f2895c
Pratfi <69865127+jhapratik2000@users.noreply.github.com>
2020-10-31 18:00:52 +0530

    created a code that prints n decimal places

---
+++ 3. Medium/nDecimalplaces.cpp
@@ -0,0 +1,7 @@
+Problem-statement:Given a number n, write a function that returns PI to n decimal places.
+Problem-link:https://edabit.com/challenge/wzPBMkanx2Db8Kpmp
+
+#include <cmath>
+double myPi(int n) {
+	return round(M_PI*pow(10, n))/pow(10, n);
+}
